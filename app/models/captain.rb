ce4596e5834879dee17fe4ffe54f807290099627
Karl Amoguis <ktamoguis@gmail.com>
2018-07-16 14:55:41 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,6 +2,8 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
+    includes(boats: :classifications)
+    .where(classifications: {name: 'Catamaran'}).uniq
   end
 
     def self.sailors
@@ -11,6 +13,7 @@ class Captain < ActiveRecord::Base
     end
 
   def self.talented_seafarers
+    includes()
   end
 
   def self.non_sailors
