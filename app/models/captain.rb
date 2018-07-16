952eaf37124ff4177e9e53757f35279652a90c09
Karl Amoguis <ktamoguis@gmail.com>
2018-07-16 13:24:36 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -7,7 +7,7 @@ class Captain < ActiveRecord::Base
   def self.sailors
     binding.pry
     joins(:boats)
-    .joins(:classifications)
+    .joins(:boat_classifications)
     .where("classifications.name = ?", 'Sailboat')
   end
 
